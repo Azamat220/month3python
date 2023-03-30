0c9c31878d9b739ded384fa493284c40f6b1f526
Azamat220 <chambulov2019@gmail.com>
2023-03-30 19:21:19 +0600

    hw6

--- handlers/client.py
+++ handlers/client.py
@@ -3,6 +3,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import bot
 from keyboards.client_kb import start_markup
 from data_base.bot_dp import sql_command_random
+from . import news
 
 
 async def start_command(message: types.Message):
@@ -52,8 +53,22 @@ async def get_random_user(message: types.Message):
                 f"@{random_user[5]}"
     )
 
+
+async def get_news(message: types.Message):
+    news_list = news.parser()
+    for i in news_list:
+        await message.answer(
+            f"{i['link']}\n\n"
+            f"<b><a href='{i['link']}'>{i['title']}</a></b>\n"
+            f"{i['description']}\n"
+            f"{i['date_from_html']}\n",
+            # parse_mode=ParseMode.HTML
+        )
+
+
 def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(start_command, commands=['start'])
     dp.register_message_handler(help_command, commands=['help'])
     dp.register_message_handler(quiz_1, commands=['quiz'])
     dp.register_message_handler(get_random_user, commands=['get'])
+    dp.register_message_handler(get_news, commands=['news'])
